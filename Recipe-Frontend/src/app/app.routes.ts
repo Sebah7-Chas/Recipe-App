39ddafde2bb772ac84889e03338b4829fcea5293
Sebah Abubeker <sebahismail21@gmail.com>
2024-03-22 22:09:51 +0100

    Recipe search and Api.

--- Recipe-Frontend/src/app/app.routes.ts
+++ Recipe-Frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesearchComponent } from './recipesearch/recipesearch.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+  path: 'recipes',
+  component: RecipesComponent
+ },
+ {
+  path: 'recipesearch',
+  component: RecipesearchComponent
+ }
+];
---
+++ Recipe-Frontend/src/app/recipesearch/recipesearch.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipesService } from '../services/recipes.service';
+import { RouterLink } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { filter } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-recipesearch',
+  standalone: true,
+  imports: [AsyncPipe, FormsModule],
+  templateUrl: './recipesearch.component.html',
+  styleUrl: './recipesearch.component.css'
+})
+export class RecipesearchComponent {
+
+  roundUp(num: number): number {
+    return Math.ceil(num);
+  }
+  
+  recipes?: any;
+  searchQuery: string = '';
+  cuisineType: string = '';
+  mealType: string = '';
+  diet: string = '';
+
+  constructor(private recipeService: RecipesService){}
+
+  searchRecipes(){
+    this.recipeService.getRecipes(this.searchQuery, this.cuisineType, this.mealType, this.diet).subscribe(res=>{
+      console.log(res);
+
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+        return {
+          selfrefrence: item._links.self.href,
+          title: item.recipe.label,
+          image: item.recipe.image,
+          totalTime: item.recipe.totalTime,
+          ingredientLines: item.recipe.ingredientLines,
+          calories: item.recipe.calories,
+        }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+}
