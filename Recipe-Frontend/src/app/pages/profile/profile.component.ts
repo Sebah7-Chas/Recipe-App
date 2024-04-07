1abae20666a7ffc6d23e8f9753971bfe317a9952
Sebah Abubeker <sebahismail21@gmail.com>
2024-04-07 04:18:09 +0200

    profile fix

--- Recipe-Frontend/src/app/pages/profile/profile.component.ts
+++ Recipe-Frontend/src/app/pages/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -10,8 +11,10 @@ import { AuthService } from '../../services/auth.service';
 })
 export class ProfileComponent {
 
-  constructor(private auth: AuthService) { }
-    logout(){
+  constructor(private auth: AuthService, private router: Router) { }
+  logout(){
+      
+    this.router.navigate(['/login']);
       this.auth.logoutUser();
     }
   }
